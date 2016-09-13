4f7eefa7ef27f1eecfc20ea8f36f28e8581fe0e1
Caleb Stevenson and Karen Freeman-Smith <caleb+karen@>
2016-09-13 13:02:36 -0700

    add instanceOf Tune test, success

--- src/test/java/TuneTest.java
+++ src/test/java/TuneTest.java
@@ -2,5 +2,15 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class TuneTest {
+  private Tune mTune;
 
+  @Before
+  public void initialize() {
+    mTune = new Tune();
+  }
+
+  @Test
+  public void Tune_instantiatesTune_true() {
+    assertEquals(true, mTune instanceof Tune);
+  }
 }
